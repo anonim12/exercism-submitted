2980f54cec8b61a810647448123a7b1d66824a81
adjiD <adjidwir@gmail.com>
2016-01-17 13:26:27 +0700

    bob first iteration

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -4,5 +4,11 @@
 
 
 def hey(what):
-
-    return
+    what = what.strip()
+    if what is None or len(what.strip()) == 0:
+        return "Fine. Be that way!"
+    elif what.isupper():
+        return "Whoa, chill out!"
+    elif what.endswith("?"):
+        return "Sure."
+    return "Whatever."
